81cf91f69d93b885980a34182622c2ff5e85fbad
Caban <netxshadow@gmail.com>
2022-09-16 11:37:13 +0000

    storage commit last

--- src/api/caban/caban_storage.erl
+++ src/api/caban/caban_storage.erl
@@ -8,6 +8,9 @@
   get_worker_value/1
 ]).
 
+% ==============================================================================================
+% ===
+% ==============================================================================================
 get_all_workers() ->
   Storage = erl_template_app:get_storage_name(),
   case Storage of
@@ -16,20 +19,21 @@ get_all_workers() ->
     _         ->  ok
   end.
 % ==============================================================================================
-% ===
-% ==============================================================================================
 get_all_workers(postgres) ->
   db_postgres:query(epgsql_pool_master, get_workers_values, []).
+% ==============================================================================================
 get_all_workers(redis, N, WorkersList) when ( N > 0 )  ->
       db_redis:q(["MSET" | [N, 0]]),
       WorkersList1 =[[{ <<"worker_name">>, integer_to_binary(N) }, { <<"worker_value">>, <<"0">> }] | WorkersList],
       get_all_workers(redis, N-1, WorkersList1);
+% ==============================================================================================
 get_all_workers(redis, _, WorkersList)  ->  WorkersList.
 % ==============================================================================================
 % ===
 % ==============================================================================================
 set_worker_value(Worker, Value) ->
     set_worker_value(Worker, Value, erl_template_app:get_storage_name()).
+% ==============================================================================================
 set_worker_value(Worker, Value, postgres) ->
   Result = db_postgres:query(epgsql_pool_master, set_worker_value,
     [ { <<"worker_value">>, integer_to_list(Value) },
@@ -37,6 +41,7 @@ set_worker_value(Worker, Value, postgres) ->
     ]),
   log:info("Set worker postgres: ~p ~n", [Result]),
   Result;
+% ==============================================================================================
 set_worker_value(Worker, Value, redis) ->
   Result = db_redis:q(["MSET" | [Worker, Value]]),
   log:info("Set worker redis: ~p ~n", [Result]),
@@ -52,6 +57,7 @@ get_worker_value(Worker, postgres) ->
     [{ <<"worker_name">>, integer_to_list(Worker) }]),
   log:info("Get worker postgres: ~p ~n", [Result]),
   Result;
+% ==============================================================================================
 get_worker_value(Worker, redis) ->
   Result = db_redis:q(["MGET" | [Worker]]),
   log:info("Get worker redis: ~p ~n", [Result]),
